cb91bc15bc3df3771f73a69780da2cfb2b384002
Modnica12 <jcycu12@gmail.com>
2020-04-28 00:18:34 +0500

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+###DFS algorithm to find linked components in graph.
+
+pass input and output filenames as script arguments
+
+first line - number of components
+
+other lines - components, they are separeted with 0
+
+input file - number of nodes, and adjacency matrix
---
+++ linked_components.py
@@ -0,0 +1,52 @@
+import sys
+
+
+input_filename = sys.argv[1]
+output_filename = sys.argv[2]
+input_file = open(input_filename, 'r')
+output_file = open(output_filename, 'w')
+
+n = int(input_file.readline())
+links = [0] * (n + 1)
+visited = [False] * (n + 1)
+components = []
+
+
+def write_result():
+    output_file.write("{}\n".format(len(components)))
+    for comp in components:
+        output_file.write(" ".join(map(str, sorted(comp))) + " 0\n")
+
+
+def dfs(v):
+    component = []
+    s = [v]
+    visited[v] = True
+    component.append(v)
+    while s:
+        current = s.pop()
+        for i in range(1, n + 1):
+            w = links[current][i - 1]
+            if w and not visited[i]:
+                visited[i] = True
+                s.append(i)
+                component.append(i)
+    print(component)
+    components.append(component)
+
+
+def main():
+    for i in range(1, n + 1):
+        current_node = list(map(int, input_file.readline().split()))
+        links[i] = current_node
+    print(links)
+    for v in range(1, n + 1):
+        if not visited[v]:
+            dfs(v)
+    write_result()
+    input_file.close()
+    output_file.close()
+
+
+if __name__ == '__main__':
+    main()
